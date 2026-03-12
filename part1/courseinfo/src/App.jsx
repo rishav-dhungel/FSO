b21373004709e60450929a0e09bec241ea3d9360
rishav <rishavdhungel1@gmail.com>
2026-03-12 20:04:39 +0545

    feat(courseinfo): complete courseinfo project

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,68 @@
+
+const App = () =>{
+
+  const course = {
+    name:  'Half Stack application development',
+    parts:[
+      {
+        name:'Fundamentals of React',
+        exercises:10,
+        id:1
+      },
+      {
+        name:'Using props to pass data',
+        exercises: 7,
+        id:2
+      },
+      {
+        name:'State of a component',
+        exercises: 14,
+        id:3
+      }
+    ]
+}
+  return(
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return(
+    <>
+    <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <>
+    {props.parts.map((part) => (<Part key = {part.id} part = {part.name} exercise ={part.exercises}/>))}
+
+    </>
+  )
+}
+
+
+const Total = (props) =>{
+  const total = props.parts.reduce((sum,part) =>{
+    return sum + part.exercises
+  },0)
+  return(
+    <>
+      <p>Number of exercies {total}</p>
+    </>
+  )
+
+}
+
+const Part = (props) =>{
+  return(
+      <p key={props.id}>{props.part} {props.exercise}</p>
+  )
+}
+export default App
